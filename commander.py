4cb7aad843e96dbfa180ed0c7a11fc5fdeb4a50a
Kevin Haghi <kevin.haghi@gmail.com>
2020-06-10 17:31:04 -0700

    Add files via upload

--- commander.py
+++ commander.py
@@ -55,37 +55,4 @@ class March:
     
     
 
-primary = 'Richard'
-secondary = 'Charles'
-build_Richard = [1,0,0,0]
-build_Charles = [1,0,0,0]
-tech = [70,70,40,70,70,40,70,70,40]
-alliance_tech = [15,15,0,15,15,0,15,15,0]
-titles = [0,0,0,0,0,0,0,0,0]
-holy_sites = [6,6,5,6,6,5,6,6,5]
-building = [2,2,6,2,2,6,2,2,6]
-vip = 15
-troop_amount = 200000
 
-
-
-kevin = Character('Ottoman')
-print(kevin.aa,kevin.sh,kevin.buffs_ca,kevin.buffs_id,kevin.buffs_skill,kevin.buffs_rally,kevin.ca)
-#kevin.civilization('Germany')
-#print(kevin.aa,kevin.sh,kevin.buffs_ca,kevin.buffs_id,kevin.buffs_skill,kevin.buffs_rally,kevin.ca)
-kevin.city_theme_buffs('No Place Like Home')
-print(kevin.aa,kevin.sh,kevin.buffs_ca,kevin.buffs_id,kevin.buffs_skill,kevin.buffs_rally,kevin.ca)
-kevin.technology_buffs(tech)
-print(kevin.aa,kevin.sh,kevin.buffs_ca,kevin.buffs_id,kevin.buffs_skill,kevin.buffs_rally,kevin.ca)
-kevin.alliance_buffs(alliance_tech)
-print(kevin.aa,kevin.sh,kevin.buffs_ca,kevin.buffs_id,kevin.buffs_skill,kevin.buffs_rally,kevin.ca)
-kevin.alliance_buffs(holy_sites)
-print(kevin.aa,kevin.sh,kevin.buffs_ca,kevin.buffs_id,kevin.buffs_skill,kevin.buffs_rally,kevin.ca)
-kevin.building_buffs(building)
-print(kevin.aa,kevin.sh,kevin.buffs_ca,kevin.buffs_id,kevin.buffs_skill,kevin.buffs_rally,kevin.ca)
-kevin.vip_buffs(vip)
-print(kevin.aa,kevin.ad,kevin.ah,kevin.buffs_ca,kevin.buffs_id,kevin.buffs_skill,kevin.buffs_rally,kevin.ca)
-print(kevin.buffs_ia,kevin.buffs_id,kevin.buffs_ih,kevin.buffs_ca,kevin.buffs_cd,kevin.buffs_ch,kevin.buffs_aa,kevin.buffs_ad,kevin.buffs_ah,kevin.buffs_skill,kevin.buffs_rally,kevin.buffs_counter)
-kevin.change_civ('Germany')
-print(kevin.aa,kevin.ad,kevin.ah,kevin.buffs_ca,kevin.buffs_id,kevin.buffs_skill,kevin.buffs_rally,kevin.ca)
-print(kevin.buffs_ia,kevin.buffs_id,kevin.buffs_ih,kevin.buffs_ca,kevin.buffs_cd,kevin.buffs_ch,kevin.buffs_aa,kevin.buffs_ad,kevin.buffs_ah,kevin.buffs_skill,kevin.buffs_rally,kevin.buffs_counter)
